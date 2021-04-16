edf6475d5846d4cc4d087a0a0c5d3ec5e430b915
Nick Lontorfos <nlontorfos@hotmail.com>
2021-04-16 12:38:27 +0200

    login component ready

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const users = [
+  {
+    isLoggedIn: true,
+    login: 'NickLont',
+    username: 'NickLont',
+    password: '123123',
+    avatarUrl: 'https://avatars.githubusercontent.com/u/17071000?v=4'
+  }
+]
+
 export default (req, res) => {
   res.status(200).json({ name: 'John Doe' })
 }
---
+++ pages/login.js
@@ -0,0 +1,15 @@
+import { Typography } from '@material-ui/core'
+import Login from 'components/Login'
+
+const LoginPage = () => {
+  return (
+    <div>
+      <Typography color="primary" variant="h5">
+        LoginPage
+      </Typography>
+      <Login />
+    </div>
+  )
+}
+
+export default LoginPage
